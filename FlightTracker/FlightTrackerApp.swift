190b6f37b343b2ec0f32f4e97e45e5ab6eade23e
John Zheng <bluevisor@gmail.com>
2025-12-02 02:35:45 -0500

    Initial Commit

---
+++ FlightTracker/FlightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTrackerApp.swift
+//  FlightTracker
+//
+//  Created by John Zheng on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FlightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlightTrackerTests/FlightTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlightTrackerTests.swift
+//  FlightTrackerTests
+//
+//  Created by John Zheng on 12/2/25.
+//
+
+import Testing
+@testable import FlightTracker
+
+struct FlightTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
